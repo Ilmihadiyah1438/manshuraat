61790dba1f3d17c3dd296db5a397147c24fc50df
Abdeali Jamali <abdealikhurrum@gmail.com>
2014-08-24 18:40:51 +0530

    added new file

---
+++ structure.py
@@ -0,0 +1,17 @@
+"""
+This file is intended to hold the basic structure of the manshuraat program.
+It defines the basic data structure of a manshuraat entry.
+Information required:
+*Name of book/article/newsletter + vol. no.
+*Name of author, publisher
+*Date and place of publishing
+*Language
+*Pages
+*Tags
+*Subtopics
+
+"""
+class Manshurat(object):
+    pass
+class Book(Manshurat):
+    pass
